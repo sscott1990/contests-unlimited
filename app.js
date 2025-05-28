f4130cd238d4fa8061e7cd15e0c13ae3398cf01a
Steven Scott <stevenscottfarms@gmail.com>
2025-05-28 04:02:22 -0400

    updated app.js for larger files

--- app.js
+++ app.js
@@ -39,7 +39,7 @@ app.use((req, res, next) => {
 const upload = multer({
   storage: multer.memoryStorage(),
   limits: {
-    fileSize: 5 * 1024 * 1024, // 5 MB limit
+    fileSize: 10 * 1024 * 1024, // 10 MB limit
   },
 });
 
